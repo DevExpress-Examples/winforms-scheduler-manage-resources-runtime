53019ec41c0f2833a2d1e2e42aa4ad5a6e4139c6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to manage scheduler resources at runtime
+
+
+<p>This example shows how you can manage scheduler resources at runtime. The Scheduler context menu is modified to include commands to create, modify or delete selected resource. To invoke the menu, right-click any region of SchedulerControl (except appointments). A custom form is invoked that enables you to set caption, color and image for the selected resource.</p>
+
+<br/>
+
+
---
+++ VB/Form1.vb
@@ -0,0 +1,119 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Windows.Forms
+Imports DevExpress.XtraScheduler
+
+Namespace SchedulerResourcesManagement
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			' TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
+			carsTableAdapter.Fill(carsDBDataSet.Cars)
+			' TODO: This line of code loads data into the 'carsDBDataSet.CarScheduling' table. You can move, or remove it, as needed.
+			carSchedulingTableAdapter.Fill(carsDBDataSet.CarScheduling)
+
+			AddHandler carSchedulingTableAdapter.Adapter.RowUpdated, AddressOf carSchedulingTableAdapter_RowUpdated
+			AddHandler carsTableAdapter.Adapter.RowUpdated, AddressOf carsTableAdapter_RowUpdated
+
+			schedulerControl1.OptionsView.ResourceHeaders.RotateCaption = False
+			schedulerControl1.OptionsBehavior.SelectOnRightClick = True
+
+			If schedulerStorage1.Appointments.Count > 0 Then
+				schedulerControl1.Start = schedulerStorage1.Appointments(0).Start
+			End If
+		End Sub
+
+		#Region "Data-Related Events"
+
+		Private Sub Appointments_Modified(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs) Handles schedulerStorage1.AppointmentsChanged, schedulerStorage1.AppointmentsInserted, schedulerStorage1.AppointmentsDeleted
+			carSchedulingTableAdapter.Update(carsDBDataSet)
+		End Sub
+
+		Private Sub Resources_Modified(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs) Handles schedulerStorage1.ResourcesDeleted, schedulerStorage1.ResourcesChanged, schedulerStorage1.ResourcesInserted
+			carsTableAdapter.Update(carsDBDataSet)
+		End Sub
+
+		Private Sub carSchedulingTableAdapter_RowUpdated(ByVal sender As Object, ByVal e As OleDbRowUpdatedEventArgs)
+			If e.Status = UpdateStatus.Continue AndAlso e.StatementType = StatementType.Insert Then
+				Dim id As Integer = 0
+				Using cmd As New OleDbCommand("SELECT @@IDENTITY", carSchedulingTableAdapter.Connection)
+					id = CInt(Fix(cmd.ExecuteScalar()))
+				End Using
+				e.Row("ID") = id
+			End If
+		End Sub
+
+		Private Sub carsTableAdapter_RowUpdated(ByVal sender As Object, ByVal e As OleDbRowUpdatedEventArgs)
+			If e.Status = UpdateStatus.Continue AndAlso e.StatementType = StatementType.Insert Then
+				Dim id As Integer = 0
+				Using cmd As New OleDbCommand("SELECT @@IDENTITY", carsTableAdapter.Connection)
+					id = CInt(Fix(cmd.ExecuteScalar()))
+				End Using
+				e.Row("ID") = id
+			End If
+		End Sub
+
+		#End Region ' Data-Related Events
+
+        Private Sub schedulerControl1_PopupMenuShowing(ByVal sender As System.Object, ByVal e As DevExpress.XtraScheduler.PopupMenuShowingEventArgs) Handles schedulerControl1.PopupMenuShowing
+            If e.Menu.Id = DevExpress.XtraScheduler.SchedulerMenuItemId.DefaultMenu Then
+                Dim itemNewResource As New SchedulerMenuItem("New Resource", AddressOf OnAddResource)
+                Dim itemEditResource As New SchedulerMenuItem("Edit Resource", AddressOf OnEditResource)
+                Dim itemDeleteResource As New SchedulerMenuItem("Delete Resource", AddressOf OnDeleteResource)
+                Dim baseIndex As Integer = 4
+
+                itemNewResource.BeginGroup = True
+                e.Menu.Items.Insert(baseIndex, itemNewResource)
+                e.Menu.Items.Insert(baseIndex + 1, itemEditResource)
+                e.Menu.Items.Insert(baseIndex + 2, itemDeleteResource)
+            End If
+        End Sub
+
+		Private Sub OnAddResource(ByVal sender As Object, ByVal e As EventArgs)
+			Dim resource As Resource = schedulerStorage1.CreateResource(-1)
+
+			Using form As New ResourceForm(resource)
+				If form.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK Then
+					schedulerStorage1.Resources.Add(resource)
+					schedulerStorage1.RefreshData()
+					ScrollToLastResource()
+				Else
+					resource.Dispose()
+				End If
+			End Using
+		End Sub
+
+		Private Sub OnEditResource(ByVal sender As Object, ByVal e As EventArgs)
+			Dim resource As Resource = schedulerControl1.SelectedResource
+
+			Using form As New ResourceForm(resource)
+				form.ShowDialog(Me)
+			End Using
+		End Sub
+
+		Private Sub OnDeleteResource(ByVal sender As Object, ByVal e As EventArgs)
+			Dim found As Appointment = schedulerStorage1.Appointments.Items.Find(AddressOf IsAssignedToCurrentResource)
+
+			If found Is Nothing Then
+				schedulerControl1.SelectedResource.Delete()
+			Else
+				MessageBox.Show("Cannot delete resource: it has appointments assigned to it.", "Message")
+			End If
+		End Sub
+
+		Private Function IsAssignedToCurrentResource(ByVal apt As Appointment) As Boolean
+			Return (apt.ResourceId Is Resource.Empty OrElse Convert.ToInt32(apt.ResourceId) = Convert.ToInt32(schedulerControl1.SelectedResource.Id))
+		End Function
+
+		Private Sub ScrollToLastResource()
+			schedulerControl1.ActiveView.FirstVisibleResourceIndex = Math.Max(0, schedulerStorage1.Resources.Count - schedulerControl1.ActiveView.ResourcesPerPage)
+			schedulerControl1.Services.Selection.SelectedResource = schedulerStorage1.Resources(schedulerStorage1.Resources.Count - 1)
+		End Sub
+    End Class
+End Namespace
